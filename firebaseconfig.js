594e25a4cf0318425d8c656b74c0df644e78e5cf
aidellordillu18 <aidellordillu@gmail.com>
2026-09-24 18:05:21 +0700

    Add files via upload

---
+++ firebaseconfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAqtBsZBVtWTPodb84VL0Iehm3LB5q4B6M",
+  authDomain: "crud-aidel.firebaseapp.com",
+  projectId: "crud-aidel",
+  storageBucket: "crud-aidel.firebasestorage.app",
+  messagingSenderId: "854394364907",
+  appId: "1:854394364907:web:fd1862e6f4d63b7a552fae",
+  measurementId: "G-S195E3M6DE"
+};
